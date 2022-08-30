740be1b0c55a45ea7196154bc26a3c6dc64ff672
Tej Pratap <bankebihari5463@gmail.com>
2022-08-30 16:59:35 +0530

    test api for test purpose

---
+++ models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstname:{
+        type:String,
+   },
+   lastname:{
+        type:String,
+   },
+   
+    email:{
+        type:String,
+        required:[true,"Please Provide the Blog body"]
+    },
+    password:{
+        type:String,
+        required:[true,"password is required"]    
+    },
+    confirmpassword:{
+        type:String,
+        
+    }
+   
+});
+
+const Blog = mongoose.model('User',userSchema);
+
+module.exports = Blog;
